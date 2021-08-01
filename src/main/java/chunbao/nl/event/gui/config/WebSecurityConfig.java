b56ac7f199a75eb57dbe6959a8e316f3b701a410
AMS\Yang_C <chunbao.yang@schiphol.nl>
2021-08-01 20:53:54 +0200

    register page

--- src/main/java/chunbao/nl/event/gui/config/WebSecurityConfig.java
+++ src/main/java/chunbao/nl/event/gui/config/WebSecurityConfig.java
@@ -12,8 +12,18 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated();
-    http.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+    http.authorizeRequests()
+        .antMatchers("/register")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout()
+        .permitAll();
   }
 
   @SuppressWarnings("deprecation")
--- src/main/java/chunbao/nl/event/gui/controller/UserController.java
+++ src/main/java/chunbao/nl/event/gui/controller/UserController.java
@@ -1,9 +1,12 @@
 package chunbao.nl.event.gui.controller;
 
 import chunbao.nl.event.gui.auth.AuthenticationUtil;
+import chunbao.nl.event.gui.model.MyUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -22,5 +25,16 @@ public class UserController {
   public String hello(Model model) {
     model.addAttribute("username", AuthenticationUtil.getAuthenticatedPrincipal());
     return "hello";
+  }
+
+  @GetMapping("/register")
+  public String registerPage(Model model) {
+    model.addAttribute("myUser", new MyUser("username", "password"));
+    return "register";
+  }
+
+  @PostMapping("/register")
+  public String register(@ModelAttribute MyUser myUser) {
+    return "login";
   }
 }
