41710cc9d05abf89c45521f486a341c6b1ab18f0
AMS\Yang_C <chunbao.yang@schiphol.nl>
2021-08-01 18:38:15 +0200

    no encrypted pw

--- src/main/java/chunbao/nl/event/gui/config/WebSecurityConfig.java
+++ src/main/java/chunbao/nl/event/gui/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package chunbao.nl.event.gui.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +14,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests().anyRequest().authenticated();
     http.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+  }
+
+  @SuppressWarnings("deprecation")
+  @Bean
+  public static NoOpPasswordEncoder passwordEncoder() {
+    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
   }
 }
